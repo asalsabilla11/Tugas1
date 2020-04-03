435a9b910ef4b1263bd083a97c9745110cff3de4
annisa salsabilla <annisasalsa27@gmail.com>
2020-04-04 02:10:54 +0700

    membuat project awal

---
+++ app/src/main/java/com/example/akb/Kode.kt
@@ -0,0 +1,12 @@
+package com.example.akb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Kode : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kode)
+    }
+}
--- app/src/main/java/com/example/akb/MainActivity.kt
+++ app/src/main/java/com/example/akb/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.akb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+/*Tanggal Pengerjaan 3 april 2020
+* Nama : Annisa Salsabilla
+* NIM : 10117196
+* Kelas : IF-5/
+
+ */
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_mulai.setOnClickListener(){ it: View!
+            val intent = intent(this,Kode::class.java)
+        }
     }
 }
